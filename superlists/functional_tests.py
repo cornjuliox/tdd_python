4ce874c7b613ce5a8f99015fb04031a0b97cf8d9
Enrico Tuvera Jr <enricojr.tuvera@gmail.com>
2018-02-11 16:49:59 +0800

    Extended functional tests - they are failing as expected.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,5 +1,8 @@
-from selenium import webdriver
+import time
 import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -10,31 +13,46 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_list_retrieve_later(self):
+        # fictional user 'edith' visits website
         self.browser.get('http://localhost:8000')
+
+        # she notices that the word 'to-do' in the title
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # she is invited to enter a to-do item straight away.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # she types 'Buy peacock feathers' into a text box
+        inputbox.send_keys('Buy peacock feathers')
+
+        # she hits Enter, the page updates, and now the page lists
+        # '1: Buy peacock feathers' as an item in a to-do list.
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # there is still a text box inviting her to add another item.
+        # she enters 'Use peacock feathers to make a fly'
         self.fail('Finish the test!')
 
-# fictional user 'edith' visits website
+        # the page updates again, and now shows both items on her list.
 
-# she notices that the word 'to-do' in the title
+        # she sees that the site has generated a unique URL for her.
 
-# she is invited to enter a to-do item straight away.
+        # she visits that URL - her to-do list is still there.
 
-# she types 'Buy peacock feathers' into a text box
-
-# she hits Enter, the page updates, and now the page lists
-# '1: Buy peacock feathers' as an item in a to-do list.
-
-# there is still a text box inviting her to add another item.
-# she enters 'Use peacock feathers to make a fly'
-
-# the page updates again, and now shows both items on her list.
-
-# she sees that the site has generated a unique URL for her.
-
-# she visits that URL - her to-do list is still there.
-
-# she closes the page
+        # she closes the page
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
