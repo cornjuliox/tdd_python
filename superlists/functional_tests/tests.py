c939dc3be93f9f4fd09c92e949833bddaa750c3a
Enrico Jr Tuvera <enricojr.tuvera@gmail.com>
2018-02-17 16:02:13 +0800

    changed check_for_row_in_list_table to wait_for_row_in_list_table
    not that I had a problem with it before, I'm just foll,owing the book as closely as I can.

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,11 +1,12 @@
 import time
-import unittest
 
 from django.test import LiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
@@ -14,10 +15,19 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                else:
+                    time.sleep(0.5)
 
     def test_can_start_list_retrieve_later(self):
         # fictional user 'edith' visits website
@@ -43,7 +53,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(3)
 
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # there is still a text box inviting her to add another item.
         # she enters 'Use peacock feathers to make a fly'
@@ -53,8 +63,8 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(3)
 
         # the page updates again, and now shows both items on her list.
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # she sees that the site has generated a unique URL for her.
         self.fail('Finish the test!')
